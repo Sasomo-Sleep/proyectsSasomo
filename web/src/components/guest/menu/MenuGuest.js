c88cf8d04cff9df49c68bca6b62667b8dfcb9544
Edgar HiSo <edghiso@gmail.com>
2021-09-16 11:58:06 +0200

    adding style

--- web/src/components/guest/menu/MenuGuest.js
+++ web/src/components/guest/menu/MenuGuest.js
@@ -1,46 +1,61 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthContext'
+import './MenuGuest.css'
 
 const MenuGuest = () => {
 
     const auth = useContext(AuthContext)
     if (!auth.user) return <> </>
+
     return (
         <div>
-            <div className="m-4 row">
-                <div >
-                    <img src={auth?.user?.avatar} alt={auth?.user?.name} />
+            <div className="menu-guest">
+                <div className="hight-panel m-4">
+                    <div className="panel">
+                        <img src={auth?.user?.avatar} alt={auth?.user?.name} className="avatar align-self-start  w-100 rounded-circle me-3" />
+                        <Link to={'/profile'} >Watch profile</Link>
+                    </div>
+                    <div className="my-3 mx-3">
+                        <h1>{auth.user.name}</h1>
+                    </div>
                 </div>
-                <div>
-                    <p>{auth.user.name}</p>
-                    <Link to={'/profile'}>Watch profile</Link>
-                </div>
-                <hr />
+                <hr id="one" />
             </div>
-            <div className="m-4 row">
-                <span>ACOUNT SETTINGS</span>
-                <div>
-                    <p>Personal Information</p>
 
-                </div>
+            <div className="m-4 ">
                 <div>
-                    <p>Payments and Collections</p>
-
-                </div>
-                <div>
-                    <p>Personal Information</p>
-
+                    <p>ACOUNT SETTINGS</p>
+                    <div className="me-auto d-flex">
+                        <span>Personal Information</span>
+                        <i class="far fa-user"></i>
+                    </div>
+                    <hr className="hr-main" />
+                    <div className="me-auto d-flex">
+                        <span>Payments and Collections</span>
+                        <i class="fab fa-cc-paypal"></i>
+                    </div>
+                    <hr className="hr-main" />
+                    <div className="me-auto d-flex">
+                        <span>Notifications</span>
+                        <i class="fas fa-bell"></i>
+                    </div>
+                    <hr className="hr-main" />
                 </div>
             </div>
-            <div className="m-4 row">
-                <span>GUEST</span>
-                <div>
-                    <p>Use as Host</p>
+
+            <div className="m-4 ">
+                <span>HOST</span>
+                <div className="me-auto d-flex">
+                    <span>Use as Host</span>
+                    <i className="fas fa-arrows-alt-h"></i>
                 </div>
-                <div>
-                    <p>Offer an Experience</p>
+                <hr className="hr-main" />
+                <div className="me-auto d-flex">
+                    <span>Offer an Experience</span>
+                    <i className="fas fa-campground"></i>
                 </div>
+                <hr className="hr-main" />
             </div>
 
 
